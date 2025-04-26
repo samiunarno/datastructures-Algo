290eda8d7b6f2745705359eb9df7604ca73d5ec6
samiunarno <samiunarno.iub@gmail.com>
2025-04-26 22:14:30 +0600

    define array

---
+++ Basic_Problems/Array0.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //define array size !
+    int marks[5]={99,100,98,97,96};
+
+    //define array without size !
+    int price[]= {98,99,100};
+
+    cout<<marks[0];
+}
