e7f4cf1596d901f966ed3433ca052cbe7d0077fe
samiunarno <samiunarno.iub@gmail.com>
2025-05-12 00:14:50 +0600

    5/12/2025

--- Problem Solving/mergesorted.cpp
+++ Problem Solving/mergesorted.cpp
@@ -1,21 +1,57 @@
-//Merge Sorted Arrays
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
-void mergeSortedArrays(vector<int>& arr1 , vector<int>& arr2){
-    int n = arr1.size();
-    int m = arr2.size();
-    vector<int> merged(n+m);
+class Solution {
+public:
+    // Method to merge two sorted arrays
+    vector<int> mergeSortedArrays(vector<int>& arr1, vector<int>& arr2) {
+        int n = arr1.size();
+        int m = arr2.size();
+        vector<int> merged(n + m);
 
-    cout<<"Merged Array is : "<<n;
+        int i = 0, j = 0, k = 0;
 
-    
+        // Merge both arrays into merged[]
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
 
+        // Copy remaining elements from arr1
+        while (i < n) {
+            merged[k++] = arr1[i++];
+        }
 
+        // Copy remaining elements from arr2
+        while (j < m) {
+            merged[k++] = arr2[j++];
+        }
 
+        return merged;
+    }
+};
 
+int main() {
+    // Input arrays (must be sorted)
+    vector<int> arr1 = {1, 3, 5, 7};
+    vector<int> arr2 = {2, 4, 6, 8};
 
+    // Create Solution object
+    Solution s;
 
+    // Call the merge function
+    vector<int> result = s.mergeSortedArrays(arr1, arr2);
+
+    // Print the merged array
+    cout << "Merged Array is: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
